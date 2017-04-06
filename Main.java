669942c748a8bc0fd5f031e8bd5a05800593486b
itaiyifrach <itai.yifrach@gmail.com>
2017-04-06 14:36:57 +0300

    main

--- Main.java
+++ Main.java
@@ -1,6 +1,35 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        if (args.length != 6) {
+            System.out.println("Number of passed arguments is not valid");
+            return;
+        }
+
+        // getting all parameters
+        String inputImageFilename = args[1];
+        int cols = Integer.parseInt(args[2]);
+        int rows = Integer.parseInt(args[3]);
+        int energyType = Integer.parseInt(args[4]);
+        String outputImageFilename = args[5];
+
+        try
+        {
+            // read the image file
+            BufferedImage image = ImageIO.read(new File(inputImageFilename));
+
+        }
+        catch (IOException e)
+        {
+            // log the exception
+            // re-throw if desired
+        }
+
     }
 }
--- Matrix.java
+++ Matrix.java
@@ -1,27 +1,27 @@
-package graphics.hw1;
 
 final public class Matrix {
     private final int M;             // number of rows
     private final int N;             // number of columns
     private final double[][] data;   // M-by-N array
 
-    // create M-by-N matrix of 0's
-    public Matrix(int M, int N) {
-        this.M = M;
+    // create N-by-M matrix of 0's
+    public Matrix(int N, int M) {
         this.N = N;
-        data = new double[M][N];
+        this.M = M;
+        data = new double[N][M];
     }
 
     // create matrix based on 2d array
     public Matrix(double[][] data, boolean useExisting) {
-        M = data.length;
-        N = data[0].length;
+        N = data.length;
+        M = data[0].length;
         if (useExisting) {
             this.data = data;
         } else {
-            this.data = new double[M][N];
-            for (int i = 0; i < M; i++)
-                System.arraycopy(data[i], 0, this.data[i], 0, N);
+            this.data = new double[N][M];
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                    this.data[i][j] = data[i][j];
         }
     }
 
@@ -53,8 +53,8 @@ final public class Matrix {
     // create and return the transpose of the invoking matrix
     public Matrix transpose() {
         Matrix A = new Matrix(N, M);
-        for (int i = 0; i < M; i++)
-            for (int j = 0; j < N; j++)
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
                 A.data[j][i] = this.data[i][j];
         return A;
     }
@@ -62,10 +62,10 @@ final public class Matrix {
     // return C = A + B
     public Matrix plus(Matrix B) {
         Matrix A = this;
-        if (B.M != A.M || B.N != A.N) throw new RuntimeException("Illegal matrix dimensions.");
-        Matrix C = new Matrix(M, N);
-        for (int i = 0; i < M; i++)
-            for (int j = 0; j < N; j++)
+        if (B.N != A.N || B.M != A.M) throw new RuntimeException("Illegal matrix dimensions.");
+        Matrix C = new Matrix(N, M);
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
                 C.data[i][j] = A.data[i][j] + B.data[i][j];
         return C;
     }
@@ -73,18 +73,18 @@ final public class Matrix {
     // return C = A - B
     public Matrix minus(Matrix B) {
         Matrix A = this;
-        if (B.M != A.M || B.N != A.N) throw new RuntimeException("Illegal matrix dimensions.");
-        Matrix C = new Matrix(M, N);
-        for (int i = 0; i < M; i++)
-            for (int j = 0; j < N; j++)
+        if (B.N != A.N || B.M != A.M) throw new RuntimeException("Illegal matrix dimensions.");
+        Matrix C = new Matrix(N, M);
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
                 C.data[i][j] = A.data[i][j] - B.data[i][j];
         return C;
     }
 
     // print matrix to standard output
     public void show() {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++)
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++)
                 System.out.printf("%9.4f ", data[i][j]);
             System.out.println();
         }
